18b1125101814001b270d6931b2f2ac257fa56ee
One290 <lenya.dorovskoy@mail.ru>
2024-04-24 15:36:26 +0800

    ppvdtyh

--- Core.Application/VendistaApi/Dto/Responses/TerminalCommandResponseDto.cs
+++ Core.Application/VendistaApi/Dto/Responses/TerminalCommandResponseDto.cs
@@ -1,18 +1,27 @@
+using System.Text.Json.Serialization;
+
 namespace Core.Application.VendistaApi.Dto.Responses;
 
 public class TerminalCommandResponseDto
 {
+    [JsonPropertyName("id")]
     public int Id { get; set; }
 
+    [JsonPropertyName("time_created")]
     public string TimeCreated { get; set; } = "Unknown";
     
+    [JsonPropertyName("name")]
     public string Name { get; set; } = "Unknown";
     
+    [JsonPropertyName("parameter1")]
     public int Parameter1 { get; set; }
     
+    [JsonPropertyName("parameter2")]
     public int Parameter2 { get; set; }
     
+    [JsonPropertyName("parameter3")]
     public int Parameter3 { get; set; }
     
+    [JsonPropertyName("status")]
     public string Status { get; set; } = "Unknown";
 }
